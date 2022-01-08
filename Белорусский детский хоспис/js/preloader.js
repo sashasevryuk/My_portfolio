295c355253c303da6801edd9840bc470c6e3c397
sashasevryuk <97331308+sashasevryuk@users.noreply.github.com>
2022-01-09 00:04:29 +0300

    Add file Белорусский детский хоспис

---
+++ Белорусский детский хоспис/js/preloader.js
@@ -0,0 +1,9 @@
+  window.onload = function () {
+      window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 3000);
+  }
+
+
+  
